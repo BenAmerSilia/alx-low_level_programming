24cd68efb4f674fcc8ffcab2a4eea83c2670b131
BenAmerSilia <silia.ben.amer.01@gmail.com>
2023-09-07 12:52:59 -0700

    Add 6-size.6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,14 +8,6 @@
  *     *
  *      * Return: Alawys 0 (Success)
  *      */
-=======
- * main - Entry point
- *
- * Description: A program that prints with puts function
- *
- * Return: Always 0
- */
->>>>>>> 10dc98b218b46f341834f88f9aa214349cf76fd4
 
 int main(void)
 {
