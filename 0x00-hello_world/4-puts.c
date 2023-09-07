703fde7b627eaa7b88f6c1f0645664b9b699bca9
BenAmerSilia <silia.ben.amer.01@gmail.com>
2023-09-07 12:29:09 -0700

    fix 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ *  * main - Entry point
+ *  *
+ *  * Description: A program that prints with puts function
+ *  *
+ *  * Return: Alawys 0 
+ */
+
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
