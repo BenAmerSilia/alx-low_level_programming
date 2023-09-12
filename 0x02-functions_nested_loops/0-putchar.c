51af287ebf5f4a12432d74910fc09a30d0a95b41
BenAmerSilia <silia.ben.amer.01@gmail.com>
2023-09-12 13:16:36 -0700

    Still trying

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-<<<<<<< HEAD
 /**
  * main - Entry point
  *
@@ -16,32 +15,10 @@ int ch;
 
 for (ch = 0; ch < 8; ch++)
 {
-_putchar(str((ch));
-_putchar("\n");
+_putchar(str[ch]);
+_putchar('\n');
 }
 
-return(0)
+return(0);
 
-=======
-
-/**
- * main - Entry point
- *
- * Return: nothing, return void
- */
-
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-
-return (0);
->>>>>>> 197bb1e04c5f138ff069e459881b3a24f9a21532
 }
